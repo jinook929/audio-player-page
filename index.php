881b65b4dcdd49bb3c8a5de22414d712c93d8434
jinook929 <jinook929@gmail.com>
2019-05-10 23:12:33 -0400

    a page with audio players

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
+  <title>Exilic on Church</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+  <script src="audioplayerengine/jquery.js"></script>
+  <script src="audioplayerengine/amazingaudioplayer.js"></script>
+  <link rel="stylesheet" type="text/css" href="audioplayerengine/initaudioplayer-1.css">
+  <script src="audioplayerengine/initaudioplayer-1.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="topnav" id="myTopnav">
+    <a target="_blank" href="https://www.exilic.com/" class="active">EXILIC</a>
+    <a target="_blank" href="https://www.exilic.com/news">NEWS</a>
+    <a target="_blank" href="https://www.exilic.com/sermonarchive">SERMONS</a>
+    <a target="_blank" href="https://www.exilic.com/visit">ABOUT</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div>
+
+  <div class="top-image"></div>
+
+  <header>
+    <h1>We, The Church</h1>
+    <p>We're very familiar with the concept of “going” to church, but the Bible tells us that Christians “are” the church. In this short sermon series we will explore how Christianity does not revolve around my individual relationship with Jesus, but rather is centered on my being part of something so much bigger. There's no “I” in church! Join us as we learn how WE are the church and why that is so important.</p>
+  </header>
+
+  <div class="hrbox">
+    <hr>
+  </div>
+
+  <section class="card">
+    <div class="h3-container">
+      <h3>MAY 4, 2019 - A BEAUTIFUL BUILDING OF PEOPLE</h3>
+    </div>
+    <audio controls>
+      <source src="http://static1.squarespace.com/static/53d8fe16e4b0733761a2f0ed/t/5cd0685f7817f7401c6f56a9/1557162081190/We+the+Church+-+A+Beautiful+Building+of+People+1st+service.mp3/original/We+the+Church+-+A+Beautiful+Building+of+People+1st+service.mp3" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+    <p class="preacher">Preacher : Pastor Gene Joo</p>
+    <p class="passage">(Sermon Passage: <a href="https://www.biblegateway.com/passage/?search=Ephesians+2%3A19-22%3B+1+Timothy+3%3A14-15%3B+1+Pet+2%3A4-5+&version=ESV">Ephesians 2:19-22; 1 Timothy 3:14-15; 1 Peter 2:4-5</a>)</p>
+  </section>
+
+  <div class="hrbox">
+    <hr>
+  </div>
+
+  <section class="card">
+    <div class="h3-container">
+      <h3>APRIL 28, 2019 - THE BODY: STRONGER TOGETHER</h3>
+    </div>
+    <audio controls>
+      <source src="http://static1.squarespace.com/static/53d8fe16e4b0733761a2f0ed/t/5cc79ae4419202c81dba8258/1556585191931/2019-04-28+We+the+Church+-+The+Body+Stronger+Together.mp3/original/2019-04-28+We+the+Church+-+The+Body+Stronger+Together.mp3?download=true" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+    <p class="preacher">Preacher : Pastor Aaron Chung</p>
+    <p class="passage">(Sermon Passage: <a href="https://www.biblegateway.com/passage/?search=1+Corinthians+12%3A12-27&version=NIV">1 Corinthians 12:12-27</a>)</p>
+  </section>
+
+  <div class="hrbox">
+    <hr>
+  </div>
+
+  <section class="card">
+    <div class="h3-container">
+      <h3>APRIL 14, 2019 - FAMILY: LABORATORY OF LOVE</h3>
+    </div>
+    <audio controls>
+      <source src="http://static1.squarespace.com/static/53d8fe16e4b0733761a2f0ed/t/5cb3ecf5e5e5f08d0144c84f/1555295482691/Family+-+Laboratory+of+Love_01.mp3/original/Family+-+Laboratory+of+Love_01.mp3?download=true" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+    <p class="preacher">Preacher : Pastor Aaron Chung</p>
+    <p class="passage">(Sermon Passage: <a href="https://www.biblegateway.com/passage/?search=john+1%3A12-13%2C+galatians+6%3A1-2%2C+10&version=NIV">John 1:12-13 and Galatians 6:1-2, 10</a>)</p>
+  </section>
+
+  <div class="hrbox">
+    <hr>
+  </div>
+
+  <section class="card">
+    <div class="h3-container">
+      <h3>APRIL 7, 2019 - I LOVE JESUS, BUT I HATE HIS WIFE</h3>
+    </div>
+    <audio controls>
+      <source src="http://static1.squarespace.com/static/53d8fe16e4b0733761a2f0ed/t/5cab5f0ee5e5f0b640cdda4b/1554734864040/We+the+Church+-+I+Love+Jesus+But+I+Hate+His+Wife.mp3/original/We+the+Church+-+I+Love+Jesus+But+I+Hate+His+Wife.mp3?download=true" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+    <p class="preacher">Preacher : Pastor Aaron Chung</p>
+    <p class="passage">(Sermon Passage: <a href="https://www.biblegateway.com/passage/?search=Isaiah+62%3A5%3B+Hebrews+10%3A24-25%3B+2+Corinthians+11%3A2&version=NIV">Isaiah 62:5; Hebrews 10:24-25; 2 Corinthians 11:2</a>)</p>
+  </section>
+
+  <div class="hrbox">
+    <hr>
+  </div>
+
+  <section class="card">
+    <div class="h3-container">
+      <h3>DNA OF EXILIC, 2019</h3>
+      <p>- <em> Our Name, “To The Elect Exiles” </em> -</p>
+    </div>
+    <div id="amazingaudioplayer-1" style="display:block;position:relative;width:95vw;max-width:1200px;height:auto;margin:0px auto 0px;">
+      <ul class="amazingaudioplayer-audios" style="display:none;">
+        <li data-artist="Pastor Gene Joo" data-title="Our Name, “To The Elect Exiles”" data-album="WE, THE CHURCH" data-info="Sermon Passage: Ephesians 2:19-22; 1 Timothy 3:14-15; 1 Peter 2:4-5" data-image="" data-duration="2076">
+          <div class="amazingaudioplayer-source" data-src="http://static1.squarespace.com/static/53d8fe16e4b0733761a2f0ed/t/5c3cb28d0e2e72aee0fc7272/1547481754590/2019-01-13+-+The+DNA+of+Exilic+-+To+The+Elect+Exiles+-+Sermon+1.mp3/original/2019-01-13+-+The+DNA+of+Exilic+-+To+The+Elect+Exiles+-+Sermon+1.mp3?download=true" data-type="audio/mpeg"></div>
+        </li>
+      </ul>
+      <div class="amazingaudioplayer-engine"><a href="http://amazingaudioplayer.com" title="jquery music player">jquery music player</a></div>
+    </div>
+    <p class="preacher">Preacher : Doctor Ligon Duncan</p>
+    <p class="passage">(Sermon Passage: <a href="https://www.biblegateway.com/passage/?search=1+peter+1%3A1-9&version=NIV">1 Peter 1:1-9</a>)</p>
+  </section>
+
+  <div class="hrbox">
+    <hr>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
